85e07092b2b13d4436c170fc660cca5b0446955a
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-05-19 14:22:19 +0900

    Controller修正 （DTO, Pipeの追加）

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,11 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
+import { TaskPropertyDto } from './dto/task-property.dto';
+import { TaskStatusPipe } from './pipe/task-status.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -25,10 +29,11 @@ export class TasksController {
   }
 
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ) {
+  @UsePipes(ValidationPipe)
+  createTask(@Body() taskPropertyDto: TaskPropertyDto) {
+    // @UsePipeを付与したメソッドはバリデーションパイプ
+    // 今回でいうとTaskPropertyDtoで定義した値のIsNotEmpty検証が有効になる
+    const { title, description } = taskPropertyDto;
     return `createTask Success! Parameter [title:${title}, description:${description}]`;
   }
 
@@ -40,7 +45,7 @@ export class TasksController {
   @Patch('/:id')
   updateTask(
     @Param('id', ParseIntPipe) id: number,
-    @Body('status') status: string,
+    @Body('status', TaskStatusPipe) status: string,
   ) {
     return `updateTask Success! Parameter [id:${id}, status:${status}]`;
   }
