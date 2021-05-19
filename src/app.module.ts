4cac5517dba22d150219c40c0f7928b423893ae1
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-05-19 15:32:09 +0900

    DBとの接続設定を記述

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // このアプリのルートモジュール
 // importsの配列の中に他のmoduleを入れると別のmoduleをimportすることが可能
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // DBの種類
+      port: 5432, // 使用ポート
+      database: 'todoapp', // データベース名
+      host: 'localhost', // DBホスト名
+      username: 'postgres', // DBユーザ名
+      password: 'postgres', // DBパスワード
+      synchronize: true, // モデル同期（trueで同期）
+      entities: [__dirname + '/**/*.entity.{js, ts}'], // ロードするエンティティ
+    }),
+  ],
 })
 export class AppModule {}
