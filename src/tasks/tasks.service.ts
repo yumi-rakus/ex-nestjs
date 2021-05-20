465dff6f104bc4b2dcc59bd5fe5f7ae5625ac7a4
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-05-20 11:51:09 +0900

    serviceを実装

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { Repository } from 'typeorm';
+import { TaskPropertyDto } from './dto/task-property.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
+
+  async getTasks(): Promise<Task[]> {
+    return this.taskRepository.find();
+  }
+
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException();
+    }
+
+    return found;
+  }
+
+  async createTask(taskPropertyDto: TaskPropertyDto): Promise<Task> {
+    const { title, description } = taskPropertyDto;
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = 'OPEN';
+
+    try {
+      await this.taskRepository.save(task);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+
+    return task;
+  }
+
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException();
+    }
+  }
+
+  async updateTask(id: number, status: string): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await this.taskRepository.save(task);
+    return task;
+  }
+}
