bddc1f9d403439b089e5c3d0a74a1a7fcfd2330d
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-05-20 11:24:41 +0900

    環境設定を読み込むよう修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
+// import { TasksModule } from './tasks/tasks.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeormConfigService } from '../config/typeorm-config.service';
 
 // このアプリのルートモジュール
 // importsの配列の中に他のmoduleを入れると別のmoduleをimportすることが可能
 @Module({
   imports: [
-    TasksModule,
+    /**
+     * env読み込み
+     * 環境変数 NODE_ENV の値によって読み込むファイルを切り替える。
+     * default.envは後続で呼ばれる。同じ変数がある場合は先に定義されているものが優先される。
+     */
+    ConfigModule.forRoot({
+      envFilePath: [`.env/${process.env.NODE_ENV}.env`, '.env/default.env'],
+      isGlobal: true,
+    }),
 
     // TypeORM … TypeScriptで記述できるORMapper
-    TypeOrmModule.forRoot({
-      type: 'postgres', // DBの種類
-      port: 5432, // 使用ポート
-      database: 'todoapp', // データベース名
-      host: 'localhost', // DBホスト名
-      username: 'postgres', // DBユーザ名
-      password: 'postgres', // DBパスワード
-      synchronize: true, // モデル同期（trueで同期）
-      entities: [__dirname + '/**/*.entity.{js, ts}'], // ロードするエンティティ
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeormConfigService,
     }),
+
+    TasksModule,
   ],
 })
 export class AppModule {}
